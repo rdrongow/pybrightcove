94049bc9db08a8175efcbd4d8343b6458cdace0c
Ron Drongowski <ron.drongowski@zeit.de>
2010-03-08 14:16:37 +0100

    changed length of title to 255

--- pybrightcove/video.py
+++ pybrightcove/video.py
@@ -592,7 +592,7 @@ class Video(object):
     def __setattr__(self, name, value):
         msg = None
         if value:
-            if name == 'name' and len(value) > 60:
+            if name == 'name' and len(value) > 255:
                 # val = value[:60] ## Is this better?
                 msg = "Video.name must be 60 characters or less."
             if name == 'reference_id' and len(value) > 150:
